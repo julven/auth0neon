9fa4f0f13613b8b946e9fa81c87ed59da67fecc6
julven <julven_condor@yahoo.com>
2025-02-16 22:04:34 +0800

    change base url to https://julven.github.io/auth0neon/

--- declares.js
+++ declares.js
@@ -1,5 +1,6 @@
-const baseUrl = 'https://julven.epizy.com/auth0test';
+// const baseUrl = 'https://julven.epizy.com/auth0test';
 // const baseUrl = 'http://localhost/test-auth';
+const baseUrl = 'https://julven.github.io/auth0neon/';
 
 const client = 'nEBhH88sL6SZeY5Iuag9DYUfKd9teVW9';
 
